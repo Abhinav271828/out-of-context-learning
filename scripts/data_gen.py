44f60d23219241b3b0fab92339234cfd51507de6
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-05-03 20:54:31 +0530

    Regular languages data generation and data

--- scripts/data_gen.py
+++ scripts/data_gen.py
@@ -1,56 +1,117 @@
 from tqdm import tqdm
+import random
 
-def gen_dyck_n_strings(l, open, close):
+"""
+We have six regular languages, of which three have the alphabet {a, b} and three have the alphabet {a, b, c}.
+The languages are:
+- Σ = {a, b}
+    - a*b*
+    - (a|bb)*a
+    - (ab*a)*
+- Σ = {a, b, c}
+    - a*bc*
+    - ((a|b)c)*
+    - (ab[c])*
+"""
+
+MAX_LENGTH = 20
+
+def generate_L1(filename, num_samples):
     """
-    Generate valid bracket sequences of length 2l
-    using open bracket `open` and close bracket `close`.
+    Generate num_samples samples of the language L1 = a*b*.
     """
-    if l == 0:
-        return [""]
-    if l == 1:
-        return [open + close]
-    res = []
-    for i in range(l):
-        for x in gen_dyck_n_strings(i, open, close):
-            for y in gen_dyck_n_strings(l - i - 1, open, close):
-                res.append(open + x + close + y)
-    return res
+    with open('../data/' + filename, "w") as f:
+        for _ in tqdm(range(num_samples)):
+            r = random.randint(1, MAX_LENGTH)
+            sample = "a" * r + "b" * random.randint(0, MAX_LENGTH-r)
+            f.write(sample + "\n")
 
-
-def shuffle(s1, s2):
+def generate_L2(filename, num_samples):
     """
-    Return all possible ways to shuffle two strings s1 and s2
-    such that the order of characters in each string is preserved.
+    Generate num_samples samples of the language L2 = (a|bb)*a.
     """
-    l = len(s1) + len(s2)
+    with open('../data/' + filename, "w") as f:
+        for _ in tqdm(range(num_samples)):
+            if random.random() < 0.5:
+                sample = "a"
+            else:
+                sample = "bb"
+            while len(sample) < MAX_LENGTH - 1:
+                r = random.random()
+                if r < 0.33:
+                    sample += "a"
+                elif r < 0.66:
+                    sample += "bb"
+                else:
+                    break
+            sample += "a"
+            f.write(sample + "\n")  
 
-    shuffle = []
-    for i in range(2**l):
-        bits = bin(i)[2:]
-        if len(bits) < l:
-            bits = "0" * (l - len(bits)) + bits
-        if bits.count("0") != len(s1) or bits.count("1") != len(s2):
-            continue
-
-        l1 = list(s1)
-        l2 = list(s2)
-        string = "".join(l1.pop(0) if bit == "0" else l2.pop(0) for bit in bits)
-        shuffle.append(string)
-    return shuffle
-
-
-def generate_shuffle_dyck_2(length):
+def generate_L3(filename, num_samples):
     """
-    Generate all possible shuffles of two Dyck-n strings (using different
-    brackets) with total length `length`.
-    Write these strings to a file `shuffle_dyck_2.txt`.
+    Generate num_samples samples of the language L3 = (ab*a)*.
     """
-    with open("shuffle_dyck_2.txt", "w") as f:
-        for l1 in range(length + 1):
-            l2 = length - l1
-            for s1 in gen_dyck_n_strings(l1, "(", ")"):
-                for s2 in tqdm(gen_dyck_n_strings(l2, "[", "]"), desc=f"Lengths {l1}+{l2}"):
-                    for string in shuffle(s1, s2):
-                        f.write(string + "\n")
+    with open('../data/' + filename, "w") as f:
+        for _ in tqdm(range(num_samples)):
+            sample = "a"
+            r = random.gauss(3, 2)
+            sample += "b" * int(r)
+            sample += "a"
+            while len(sample) < MAX_LENGTH:
+                if random.random() > 0.4: break
+                sample += "a"
+                r = random.gauss(3, 2)
+                sample += "b" * int(r)
+                sample += "a"
+                f.write(sample + "\n")
 
-generate_shuffle_dyck_2(8)
+def generate_L4(filename, num_samples):
+    """
+    Generate num_samples samples of the language L4 = a*bc*.
+    """
+    with open('../data/' + filename, "w") as f:
+        for _ in tqdm(range(num_samples)):
+            r = random.randint(1, MAX_LENGTH-1)
+            sample = "a" * r + "b" + "c" * random.randint(0, MAX_LENGTH-1-r)
+            f.write(sample + "\n")
+
+def generate_L5(filename, num_samples):
+    """
+    Generate num_samples samples of the language L5 = ((a|b)c)*.
+    """
+    with open('../data/' + filename, "w") as f:
+        for _ in tqdm(range(num_samples)):
+            l = random.randint(1, MAX_LENGTH)
+            sample = ""
+            while len(sample) < l:
+                if random.random() < 0.5:
+                    sample += "a"
+                else:
+                    sample += "b"
+                sample += "c"
+            f.write(sample + "\n")
+
+def generate_L6(filename, num_samples):
+    """
+    Generate num_samples samples of the language L6 = (ab[c])*.
+    """
+    with open('../data/' + filename, "w") as f:
+        for _ in tqdm(range(num_samples)):
+            l = random.randint(1, MAX_LENGTH)
+            sample = ""
+            while len(sample) < l:
+                sample += "a"
+                sample += "b"
+                if random.random() < 0.5:
+                    sample += "c"
+            f.write(sample + "\n")
+
+def create_data_for(language, num_samples):
+    eval(f'generate_L{language}')(f'L{language}.txt', num_samples)
+
+#create_data_for(1, 1000000)
+#create_data_for(2, 1000000)
+#create_data_for(3, 1000000)
+#create_data_for(4, 1000000)
+#create_data_for(5, 1000000)
+#create_data_for(6, 1000000)
