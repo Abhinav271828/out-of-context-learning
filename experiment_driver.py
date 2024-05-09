e65cdd2812e7d7f8d672040ed498452ad184b051
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-05-09 16:32:12 +0530

    Cleanup

--- experiment_driver.py
+++ experiment_driver.py
@@ -16,9 +16,7 @@ def name_of_run(nheads, model_dim, num_classes, is_linear):
     return f"membership-autoencoder_no_clf-nheads={nheads}_model_dim={model_dim}_num_classes={num_classes}_is_linear={is_linear}"
 
 
-# def main():
-
-# datasets
+# First, we define datasets and dataloaders that sample from all the languages (including negative samples).
 # %%
 train_dataset = MembershipFewShot(10, 80000)
 val_dataset = MembershipFewShot(10, 10000)
@@ -30,7 +28,8 @@ train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=64)
 test_loader = DataLoader(test_dataset, batch_size=64)
 
-# model
+# Then, we define the model that will train on these datasets.
+# Note that the autoencoder is pretrained.
 # %%
 from scripts.transformer_with_auto_features import (
     MembershipModelPlusAutoEncoderLightning,
@@ -42,7 +41,6 @@ model = AutoEncoderLightning.load_from_checkpoint(
     "MGM/5a3e1n10/checkpoints/epoch=18-step=330942.ckpt"
 )
 encoder = model.model.encoder
-#clf = torch.load("classify_between_trash_and_regula.pt")
 
 model = MembershipModelPlusAutoEncoderLightning(
     string_length=20,
@@ -51,12 +49,11 @@ model = MembershipModelPlusAutoEncoderLightning(
     num_heads=1,
     lr=1e-3,
     autoencoder=encoder,
-    # regression_func=clf,
     linear_attention=True,
 )
 
+# We define the logger and the trainer objects, and train and test the model.
 # %%
-# logger
 run_name = name_of_run(1, 17, 1, True)
 config = {
     "experiment_type": "membership_encoder_no_clf",
@@ -64,28 +61,18 @@ config = {
 }
 logger = WandbLogger(project="MGM", name=run_name, config=config)
 
-# early stopping
 early_stopping = EarlyStopping(monitor="val_loss", patience=5, mode="min", verbose=True)
 
-# trainer
 trainer = Trainer(
     max_epochs=100,
     logger=logger,
     callbacks=[early_stopping],
 )
 
-# train
 trainer.fit(model, train_loader, val_loader)
 
-# test
 trainer.test(model, test_loader)
 
 # %%
 
 logger.experiment.finish()
-
-
-# if __name__ == "__main__":
-#     main()
-
-# %%
